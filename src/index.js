311a660990161cdf39f9fa77851c9840076a4868
shep-js <kateshakurina@gmail.com>
2023-01-19 12:23:42 +0100

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arrStr = str.split("");
+  for (let i = 0; i < arrStr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let bracketsVar = bracketsConfig[j];
+      if (
+        arrStr.indexOf(bracketsVar[0]) != -1 &&
+        arrStr[i] === bracketsVar[0] &&
+        arrStr[i + 1] === bracketsVar[1]
+      ) {
+        arrStr.splice(i, 2);
+        i = -1;
+        break;
+      }
+    }
+  }
+  return arrStr.length === 0;
+};
